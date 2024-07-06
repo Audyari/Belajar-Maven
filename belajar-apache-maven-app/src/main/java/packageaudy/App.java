62a3d3026c53e187d67c41e8e019e22b18846e17
Audyari Wiyono <audy123ari@gmail.com>
2024-07-06 13:57:59 +0700

    Multi Module Project

---
+++ belajar-apache-maven-app/src/main/java/packageaudy/App.java
@@ -0,0 +1,21 @@
+package packageaudy;
+
+import com.google.gson.Gson;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+
+        Person person = new Person("Audyari Wiyono");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
